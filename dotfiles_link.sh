e61df0dcf54c751b350d590a5a216fc1354e8142
epaew <epaew.333@gmail.com>
2018-06-28 10:24:29 +0900

    shell script for environment initialize

---
+++ dotfiles_link.sh
@@ -0,0 +1,58 @@
+#!/bin/sh -x
+
+# create sym links
+FILES=$(cat << __EOF__
+.tigrc
+.vimrc
+.zshenv
+.tmux.conf
+.pryrc
+__EOF__
+)
+
+for f in $FILES; do
+    ln -sf ~/dotfiles/$f ~/$f
+done
+
+if [ "$(uname)" == 'Darwin' ]; then
+    ln -s ~/dotfiles/Brewfile ~/Brewfile && brew bundle
+elif [ "$(uname)" == 'Linux' ]; then
+    ln -s ~/dotfiles/.conkyrc ~/.conkyrc
+fi
+
+# set git global config
+(diff ~/.gitconfig ~/dotfiles/.gitconfig | grep '>' >/dev/null ) && \
+    cat ~/dotfiles/.gitconfig >> ~/.gitconfig
+
+# for nvim
+[ -d ~/.config/nvim ] || \
+    (mkdir -p ~/.config && ln -sf ~/dotfiles/config/nvim/ ~/.config/nvim)
+
+# install dependencies
+[ -d ~/.autojump ] || \
+    (git clone --depth 1 git://github.com/joelthelion/autojump.git \
+        ~/autojump && \
+    cd ~/autojump && \
+    ./install.py)
+
+[ -d ~/.tmux ] || \
+    (mkdir -p ~/.tmux/plugins && \
+    git clone --depth 1 https://github.com/tmux-plugins/tpm.git \
+        ~/.tmux/plugins/tpm)
+
+[ -d ~/.cache/dein/repos/github.com/Shougo ] || \
+    (mkdir -p ~/.cache/dein/repos/github.com/Shougo && \
+    git clone --depth 1 https://github.com/Shougo/dein.vim.git \
+        ~/.cache/dein/repos/github.com/Shougo/dein.vim)
+
+[ -d ~/.zsh-completions ] || \
+    (git clone --depth 1 git://github.com/zsh-users/zsh-completions.git \
+        ~/.zsh-completions)
+
+[ -d ~/.config/zsh/functions ] || \
+    (mkdir -p ~/.config/zsh/functions && \
+    cd ~/.config/zsh/functions && \
+    curl -LO \
+        https://raw.github.com/git/git/master/contrib/completion/git-completion.bash \
+        https://raw.github.com/git/git/master/contrib/completion/git-completion.zsh -o _git
+    )
